5ff10ef2bd019afa342eccf3f6c1bbacc8dc77a0
Mihir Singh <mihirsingh604@gmail.com>
2025-11-10 17:54:54 -0800

    horiz. and vert. flow values being collected across all frames

--- optical_flow_encoding.py
+++ optical_flow_encoding.py
@@ -5,6 +5,10 @@ ret, frame1 = cap.read()
 prvs = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
 hsv = np.zeros_like(frame1)
 hsv[..., 1] = 255
+
+allHorizontalFlowValues = []
+allVerticalFlowValues = []
+
 while(1):
     ret, frame2 = cap.read()
     if not ret:
@@ -13,6 +17,10 @@ while(1):
 
     next = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
     flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+
+    allHorizontalFlowValues.append(flow[...,0])
+    allVerticalFlowValues.append(flow[..., 1])
+
     mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
     hsv[..., 0] = ang*180/np.pi/2
     hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
@@ -28,3 +36,5 @@ while(1):
 
 cv.destroyAllWindows()
 
+print("Horizontal flow array shape:", np.array(allHorizontalFlowValues).shape)
+print("Vertical flow array shape:", np.array(allVerticalFlowValues).shape)
